174221f76ef2c2eefcbc1564de361f4fc0dc67c8
Momchil Atanasov <momchil.dev@gmail.com>
2015-03-17 22:53:51 +0200

    Add graphics API

---
+++ resources/graphics.js
@@ -0,0 +1,31 @@
+oop.namespace("graphics");
+
+graphics.Image = oop.class(resource.Resource, {
+  __create__: function(path) {
+    this.__super__();
+    this.img = new Image();
+    this.img.src = path;
+    this.img.onload = $.proxy(function() {
+      this.setLoaded(true);
+    }, this);
+  },
+  getImg: function() {
+    return this.img;
+  }
+});
+
+graphics.Renderer = oop.class({
+  __create__: function(canvas) {
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.context = canvas.getContext("2d");
+  },
+  clear: function() {
+    this.context.clearRect(0, 0, this.width, this.height);
+  },
+  drawImage: function(image, x, y) {
+    var width = image.getImg().width;
+    var height = image.getImg().height;
+    this.context.drawImage(image.getImg(), x - width / 2, y - height / 2, width, height);
+  }
+});
