40f9afe86963ec0a6ed4acb947d04100e6f44311
Momchil Atanasov <momchil.dev@gmail.com>
2015-03-31 22:07:25 +0300

    Add collision-less motion system

--- resources/entity.js
+++ resources/entity.js
@@ -44,3 +44,39 @@ brickdest.entity.MotionFeature = oop.class({
     return this.mass;
   }
 });
+
+brickdest.entity.Entity = oop.class({
+  locationFeature: null,
+  motionFeature: null,
+  collisionFeature: null,
+  renderFeature: null
+});
+
+brickdest.entity.MotionSystem = oop.class({
+  __create__: function() {
+    this.acceleration = 9.8;
+  },
+  setAcceleration: function(acceleration) {
+    this.acceleration = acceleration;
+  },
+  getAcceleration: function() {
+    return this.acceleration;
+  },
+  process: function(entity, elapsedSeconds) {
+    if (!entity.motionFeature) {
+      return;
+    }
+    var speedX = entity.motionFeature.getSpeedX();
+    var speedY = entity.motionFeature.getSpeedY();
+    var newSpeedY = speedY + this.acceleration * elapsedSeconds;
+
+    var locationX = entity.locationFeature.getX();
+    var locationY = entity.locationFeature.getY();
+    var newLocationX = locationX + speedX * elapsedSeconds;
+    var newLocationY = locationY + (speedY + newSpeedY) * elapsedSeconds / 2.0;
+
+    entity.locationFeature.setX(newLocationX);
+    entity.locationFeature.setY(newLocationY);
+    entity.motionFeature.setSpeedY(newSpeedY);
+  }
+});
--- spec/entity_spec.js
+++ spec/entity_spec.js
@@ -1,56 +1,110 @@
-describe("LocationFeature", function() {
+describe("entity", function() {
   var epsilon = 0.0001;
-  var feature;
 
-  beforeEach(function() {
-    feature = new brickdest.entity.LocationFeature();
+  describe("LocationFeature", function() {
+    var feature;
+
+    beforeEach(function() {
+      feature = new brickdest.entity.LocationFeature();
+    });
+
+    it("has default location is 0.0/0.0", function() {
+      expect(feature.getX()).toBeCloseTo(0.0, epsilon);
+      expect(feature.getY()).toBeCloseTo(0.0, epsilon);
+    });
+
+    it("is possible to change X coordinate", function() {
+      feature.setX(1.3);
+      expect(feature.getX()).toBeCloseTo(1.3, epsilon);
+    });
+
+    it("is possible to change Y coordinate", function() {
+      feature.setY(2.1);
+      expect(feature.getY()).toBeCloseTo(2.1, epsilon);
+    });
   });
 
-  it("has default location is 0.0/0.0", function() {
-    expect(feature.getX()).toBeCloseTo(0.0, epsilon);
-    expect(feature.getY()).toBeCloseTo(0.0, epsilon);
+  describe("MotionFeature", function() {
+    var feature;
+
+    beforeEach(function() {
+      feature = new brickdest.entity.MotionFeature();
+    });
+
+    it("has default speed 0.0/0.0", function() {
+      expect(feature.getSpeedX()).toBeCloseTo(0.0, epsilon);
+      expect(feature.getSpeedY()).toBeCloseTo(0.0, epsilon);
+    });
+
+    it("is possible to change speed along X", function() {
+      feature.setSpeedX(1.6);
+      expect(feature.getSpeedX()).toBeCloseTo(1.6, epsilon);
+    });
+
+    it("is possible to change speed along Y", function() {
+      feature.setSpeedY(8.5);
+      expect(feature.getSpeedY()).toBeCloseTo(8.5, epsilon);
+    });
+
+    it("has default mass of 1.0", function() {
+      expect(feature.getMass()).toBeCloseTo(1.0, epsilon);
+    });
+
+    it("is possible to change mass", function() {
+      feature.setMass(7.8);
+      expect(feature.getMass()).toBeCloseTo(7.8, epsilon);
+    });
   });
 
-  it("is possible to change X coordinate", function() {
-    feature.setX(1.3);
-    expect(feature.getX()).toBeCloseTo(1.3, epsilon);
-  });
+  describe("MotionSystem", function() {
+    var system;
+    var entity;
 
-  it("is possible to change Y coordinate", function() {
-    feature.setY(2.1);
-    expect(feature.getY()).toBeCloseTo(2.1, epsilon);
-  });
-});
+    beforeEach(function() {
+      system = new brickdest.entity.MotionSystem();
+      system.setAcceleration(3.4);
 
-describe("MotionFeature", function() {
-  var epsilon = 0.0001;
-  var feature;
+      entity = new brickdest.entity.Entity();
+      entity.locationFeature = new brickdest.entity.LocationFeature();
+      entity.locationFeature.setX(11.3);
+      entity.locationFeature.setY(5.4);
+    });
 
-  beforeEach(function() {
-    feature = new brickdest.entity.MotionFeature();
-  });
+    it("is possible to change acceleration", function() {
+      expect(system.getAcceleration()).toBeCloseTo(3.4);
+    });
 
-  it("has default speed 0.0/0.0", function() {
-    expect(feature.getSpeedX()).toBeCloseTo(0.0, epsilon);
-    expect(feature.getSpeedY()).toBeCloseTo(0.0, epsilon);
-  });
+    describe("when a motionless entity is processed", function() {
+      beforeEach(function() {
+        system.process(entity, 2.0);
+      });
 
-  it("is possible to change speed along X", function() {
-    feature.setSpeedX(1.6);
-    expect(feature.getSpeedX()).toBeCloseTo(1.6, epsilon);
-  });
+      it("the entity is not affected", function() {
+        expect(entity.locationFeature.getX()).toBeCloseTo(11.3, epsilon);
+        expect(entity.locationFeature.getY()).toBeCloseTo(5.4, epsilon);
+      });
+    });
 
-  it("is possible to change speed along Y", function() {
-    feature.setSpeedY(8.5);
-    expect(feature.getSpeedY()).toBeCloseTo(8.5, epsilon);
-  });
+    describe("when a movable entity is processed", function() {
+      beforeEach(function() {
+        entity.motionFeature = new brickdest.entity.MotionFeature();
+        entity.motionFeature.setSpeedX(1.0);
+        entity.motionFeature.setSpeedY(-2.0);
+        system.process(entity, 2.0);
+      });
 
-  it("has default mass of 1.0", function() {
-    expect(feature.getMass()).toBeCloseTo(1.0, epsilon);
-  });
+      it("the speed should have changed accordingly", function() {
+        // speed = v0 + a * t = -2.0 + 3.4 * 2.0
+        expect(entity.motionFeature.getSpeedX()).toBeCloseTo(1.0, epsilon);
+        expect(entity.motionFeature.getSpeedY()).toBeCloseTo(4.8, epsilon);
+      });
 
-  it("is possible to change mass", function() {
-    feature.setMass(7.8);
-    expect(feature.getMass()).toBeCloseTo(7.8, epsilon);
+      it("the location should have changed accordingly", function() {
+        // distanceX = v0 * t = 2.0
+        // distanceY = (v0 + v1) * t / 2 = (-2.0 + 4.8) * 2.0 / 2 = 2.8
+        expect(entity.locationFeature.getX()).toBeCloseTo(13.3);
+        expect(entity.locationFeature.getY()).toBeCloseTo(8.2);
+      });
+    });
   });
 });
