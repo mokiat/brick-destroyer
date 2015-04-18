0fbf9ba6cb37421fdf8e53e4cbf56522ca5244f4
Momchil Atanasov <momchil.dev@gmail.com>
2015-04-18 15:19:18 +0300

    Add Rectangle shape

--- resources/shape.js
+++ resources/shape.js
@@ -1,7 +1,16 @@
 oop.namespace("brickdest.shape");
 
 brickdest.shape.IShape = oop.interface({
+  // This should return the shortest distance to any point
+  // of the shape, not only vertices.
+  // Collision detection uses this to determine the best stepping size
+  // so that objects don't overjump each other in a single move.
   getClosestPointDistance: function() {},
+
+  // This should return the longest distance to any point
+  // of the shape, not only vertices.
+  // Collision detection uses this to do a fast check whether two objects
+  // have at all a chance of collision, without doing slow evaluations.
   getFurthestPointDistance: function() {}
 });
 
@@ -17,5 +26,39 @@ brickdest.shape.Circle = oop.class({
   },
   getFurthestPointDistance: function() {
     return this.radius;
+  }
+});
+
+brickdest.shape.Rectangle = oop.class({
+  width: 2.0,
+  height: 1.0,
+  __create__: function(config) {
+    if (config) {
+      if (config.width) {
+        this.width = config.width;
+      }
+      if (config.height) {
+        this.height = config.height;
+      }
+    }
+  },
+  getHalfWidth: function() {
+    return this.width / 2.0;
+  },
+  getHalfHeight: function() {
+    return this.height / 2.0;
+  },
+  getClosestPointDistance: function() {
+    if (this.width < this.height) {
+      return this.getHalfWidth();
+    } else {
+      return this.getHalfHeight();
+    }
+  },
+  getFurthestPointDistance: function() {
+    var halfWidth = this.getHalfWidth();
+    var halfHeight = this.getHalfHeight();
+    var distanceSquared = halfWidth * halfWidth + halfHeight * halfHeight;
+    return Math.sqrt(distanceSquared);
   }
 });
--- spec/shape_spec.js
+++ spec/shape_spec.js
@@ -32,10 +32,6 @@ describe("Shape", function() {
         circle.radius = 32.0;
       });
 
-      it("should be instance of it's constructor", function() {
-        expect(circle instanceof brickdest.shape.Circle).toBeTruthy();
-      });
-
       it("should implement the IShape interface", function() {
         expect(brickdest.shape.IShape.isImplementedBy(circle)).toBeTruthy();
       });
@@ -46,6 +42,77 @@ describe("Shape", function() {
 
       it("the furthest point distance should equal the radius", function() {
         expect(circle.getFurthestPointDistance()).toBeCloseTo(32.0, decimalPoints);
+      });
+    });
+  });
+
+  describe("Rectangle", function() {
+    var rectangle;
+
+    describe("when default rectangle is created", function() {
+      beforeEach(function() {
+        rectangle = new brickdest.shape.Rectangle();
+      });
+
+      it("has default width of 2.0", function() {
+        expect(rectangle.width).toBeCloseTo(2.0, decimalPoints);
+      });
+
+      it("has default height of 1.0", function() {
+        expect(rectangle.height).toBeCloseTo(1.0, decimalPoints);
+      });
+    });
+
+    describe("when rectangle's data is initialized via constructor", function() {
+      beforeEach(function() {
+        rectangle = new brickdest.shape.Rectangle({
+          width: 2.5,
+          height: 1.3
+        });
+      });
+
+      it("the width is configured accordingly", function() {
+        expect(rectangle.width).toBeCloseTo(2.5, decimalPoints);
+      });
+
+      it("the height is configured accordingly", function() {
+        expect(rectangle.height).toBeCloseTo(1.3, decimalPoints);
+      });
+    });
+
+    describe("given a rectangle", function() {
+      var wideRectangle;
+      var tallRectangle;
+
+      beforeEach(function() {
+        wideRectangle = new brickdest.shape.Rectangle();
+        wideRectangle.width = 4.6;
+        wideRectangle.height = 2.4;
+        tallRectangle = new brickdest.shape.Rectangle();
+        tallRectangle.width = 2.4;
+        tallRectangle.height = 4.6;
+      });
+
+      it("should implement the IShape interface", function() {
+        expect(brickdest.shape.IShape.isImplementedBy(wideRectangle)).toBeTruthy();
+      });
+
+      it("is possible to get half of the width", function() {
+        expect(wideRectangle.getHalfWidth()).toBeCloseTo(2.3, decimalPoints);
+      });
+
+      it("is possible to get half of the height", function() {
+        expect(wideRectangle.getHalfHeight()).toBeCloseTo(1.2, decimalPoints);
+      });
+
+      it("the closest point distance should equal half of the shortest side", function() {
+        expect(wideRectangle.getClosestPointDistance()).toBeCloseTo(1.2, decimalPoints);
+        expect(tallRectangle.getClosestPointDistance()).toBeCloseTo(1.2, decimalPoints);
+      });
+
+      it("the furthest point distance equal the distance to any of the corners", function() {
+        expect(wideRectangle.getFurthestPointDistance()).toBeCloseTo(2.594224354, decimalPoints);
+        expect(tallRectangle.getFurthestPointDistance()).toBeCloseTo(2.594224354, decimalPoints);
       });
     });
   });
