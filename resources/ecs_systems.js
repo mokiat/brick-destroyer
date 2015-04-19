97b69a3e416c9064994c321c0b8965c18dc334a9
Momchil Atanasov <momchil.dev@gmail.com>
2015-04-19 12:16:20 +0300

    Add collision detection to MotionSystem

--- resources/ecs_systems.js
+++ resources/ecs_systems.js
@@ -26,6 +26,7 @@ brickdest.ecs.MotionSystem = oop.class({
   gravity: new brickdest.math.Vector(),
   __create__: function(manager) {
     this.manager = manager;
+    this.collisionEvaluator = new brickdest.ecs.CollisionEvaluator();
   },
   update: function(elapsedSeconds) {
     var entities = this.manager.filterEntities(["location", "motion"]);
@@ -40,11 +41,67 @@ brickdest.ecs.MotionSystem = oop.class({
     var speedDelta = this.gravity.mul(elapsedSeconds);
     var oldSpeed = motionComp.speed;
     var newSpeed = oldSpeed.inc(speedDelta);
-    motionComp.speed = newSpeed;
-
     var deltaLocation = oldSpeed.inc(newSpeed).mul(elapsedSeconds * 0.5);
     var oldLocation = locationComp.location;
     var newLocation = oldLocation.inc(deltaLocation);
+
+    if (entity.hasComponent("collision")) {
+      // Move a fraction of the smallest dimension of the shape
+      var stepSize = 0.5 * entity.getComponent("collision").shape.getClosestPointDistance();
+      if (deltaLocation.getSquaredLength() > stepSize * stepSize) {
+        // We have moved way too much for a proper collision detection.
+        // Do a two separate half-time moves instead.
+        this.moveEntity(entity, elapsedSeconds / 2.0);
+        this.moveEntity(entity, elapsedSeconds / 2.0);
+        return;
+      }
+    }
+
+    motionComp.speed = newSpeed;
     locationComp.location = newLocation;
+
+    if (entity.hasComponent("collision")) {
+      this.checkEntityCollision(entity);
+    }
+  },
+  checkEntityCollision: function(entity) {
+    var entities = this.manager.filterEntities(["location", "collision"]);
+    for (var i = 0; i < entities.length; i++) {
+      var staticEntity = entities[i];
+      if (staticEntity != entity) {
+        this.checkCollisionBetween(staticEntity, entity);
+      }
+    }
+  },
+  checkCollisionBetween: function(staticEntity, movingEntity) {
+    var vector = this.collisionEvaluator.getEscapeVector(staticEntity, movingEntity);
+    if (vector == null) {
+      return;
+    }
+    if (staticEntity.hasComponent("motion")) {
+      throw "Collision between two moving objects not supported!";
+    }
+
+    var movingLocationComp = movingEntity.getComponent("location");
+    movingLocationComp.location.x += vector.x;
+    movingLocationComp.location.y += vector.y;
+
+    var movingMotionComp = movingEntity.getComponent("motion");
+    var staticCollisionComp = staticEntity.getComponent("collision");
+    var movingCollisionComp = movingEntity.getComponent("collision");
+
+    var totalDeflection = staticCollisionComp.deflection * movingCollisionComp.deflection;
+    var totalFriction = staticCollisionComp.friction * movingCollisionComp.friction;
+
+    var collisionNormal = vector.resize(1.0);
+    var penetrationVector = collisionNormal.mul(collisionNormal.dotProduct(movingMotionComp.speed));
+
+    var slideVector = movingMotionComp.speed.dec(penetrationVector);
+    slideVector = slideVector.mul(1.0 - totalFriction);
+
+    var bounceVector = penetrationVector.mul(-1.0);
+    bounceVector = bounceVector.mul(totalDeflection);
+
+    movingMotionComp.speed = bounceVector.inc(slideVector);
   }
 });
--- spec/ecs_systems_motion_spec.js
+++ spec/ecs_systems_motion_spec.js
@@ -2,6 +2,8 @@ describe("MotionSystem", function() {
   var decimalPoints = 4;
   var manager;
   var system;
+  var entity;
+  var obstacle;
 
   beforeEach(function() {
     manager = new brickdest.ecs.EntityManager();
@@ -15,8 +17,6 @@ describe("MotionSystem", function() {
   });
 
   describe("when entity moves in a gravity field", function() {
-    var entity;
-
     beforeEach(function() {
       system.gravity = new brickdest.math.Vector(1.2, 3.4);
 
@@ -43,6 +43,115 @@ describe("MotionSystem", function() {
       var location = entity.getComponent("location");
       expect(location.location.x).toBeCloseTo(15.7);
       expect(location.location.y).toBeCloseTo(8.2);
+    });
+  });
+
+  describe("when entity collides with a very thin obstacle", function() {
+    beforeEach(function() {
+      entity = manager.createEntity();
+
+      entity.addComponent("location", new brickdest.ecs.LocationComponent({
+        location: new brickdest.math.Vector(-4.0, -5.1)
+      }));
+
+      entity.addComponent("motion", new brickdest.ecs.MotionComponent({
+        speed: new brickdest.math.Vector(1.0, 1.0)
+      }));
+
+      entity.addComponent("collision", new brickdest.ecs.CollisionComponent({
+        shape: new brickdest.shape.Circle({
+          radius: 1.0
+        }),
+        friction: 0.0,
+        deflection: 1.0
+      }));
+
+      obstacle = manager.createEntity();
+
+      obstacle.addComponent("location", new brickdest.ecs.LocationComponent({
+        location: new brickdest.math.Vector()
+      }));
+
+      obstacle.addComponent("collision", new brickdest.ecs.CollisionComponent({
+        shape: new brickdest.shape.Rectangle({
+          width: 3.0,
+          height: 0.2
+        }),
+        friction: 0.0,
+        deflection: 1.0
+      }));
+    });
+
+    describe("when there is no friction and maximum deflection", function() {
+      beforeEach(function() {
+        manager.update(8.0);
+      });
+
+      it("the object should have changed its speed accordingly", function() {
+        var motionComp = entity.getComponent("motion");
+        expect(motionComp.speed.x).toBeCloseTo(1.0, decimalPoints);
+        expect(motionComp.speed.y).toBeCloseTo(-1.0, decimalPoints);
+      });
+
+      it("the object should have changed its location accordingly", function() {
+        var locationComp = entity.getComponent("location");
+        expect(locationComp.location.x).toBeCloseTo(4.0, decimalPoints);
+        expect(locationComp.location.y).toBeCloseTo(-5.1, decimalPoints);
+      });
+    });
+
+    describe("when deflection is lowered", function() {
+      beforeEach(function() {
+        entity.getComponent("collision").deflection = 0.5;
+        obstacle.getComponent("collision").deflection = 0.5;
+
+        manager.update(8.0);
+      });
+
+      it("the object should have changed its speed accordingly", function() {
+        var motionComp = entity.getComponent("motion");
+        expect(motionComp.speed.x).toBeCloseTo(1.0, decimalPoints);
+        expect(motionComp.speed.y).toBeCloseTo(-0.25, decimalPoints);
+      });
+
+      it("the object should have changed its location accordingly", function() {
+        var locationComp = entity.getComponent("location");
+        expect(locationComp.location.x).toBeCloseTo(4.0, decimalPoints);
+        expect(locationComp.location.y).toBeCloseTo(-2.1, decimalPoints);
+      });
+    });
+
+    describe("when friction is increased", function() {
+      beforeEach(function() {
+        entity.getComponent("collision").friction = 0.5;
+        obstacle.getComponent("collision").friction = 0.5;
+
+        manager.update(8.0);
+      });
+
+      it("the object should have changed its speed accordingly", function() {
+        var motionComp = entity.getComponent("motion");
+        expect(motionComp.speed.x).toBeCloseTo(0.75, decimalPoints);
+        expect(motionComp.speed.y).toBeCloseTo(-1.0, decimalPoints);
+      });
+
+      it("the object should have changed its location accordingly", function() {
+        var locationComp = entity.getComponent("location");
+        expect(locationComp.location.x).toBeCloseTo(3.0, decimalPoints);
+        expect(locationComp.location.y).toBeCloseTo(-5.1, decimalPoints);
+      });
+    });
+
+    describe("when obstacle is movable", function() {
+      beforeEach(function() {
+        obstacle.addComponent("motion", new brickdest.ecs.MotionComponent());
+      });
+
+      it("should throw an error", function() {
+        expect(function() {
+          manager.update(8.0);
+        }).toThrow();
+      });
     });
   });
 });
