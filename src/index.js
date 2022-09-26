208b612d8b21d2d62f1c6e0753c6e7848d3a5c28
Momchil Atanasov <momchil.dev@gmail.com>
2022-09-26 21:02:24 +0300

    Add initial index

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './components/App';
+
+import './index.css';
+
+const headless = process.env.REACT_APP_HEADLESS === 'true';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Application decorations={!headless} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
